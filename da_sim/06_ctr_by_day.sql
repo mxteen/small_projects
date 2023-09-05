5a2b452925e5d092d534f5a077765bcd75abe789
Maksim Terentev <maxim.i.terentev@gmail.com>
2023-09-06 00:40:22 +0300

    deleted da_sim: It's gonna be in rep.with courses

--- da_sim/06_ctr_by_day.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    DATE(time) AS day,
-    countIf(user_id, action='like') as likes,
-    countIf(user_id, action='view') as views,
-    likes / views as CTR
-FROM 
-    simulator_20230720.feed_actions
-GROUP BY 
-    day
