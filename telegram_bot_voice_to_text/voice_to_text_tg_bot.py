48cb8c0994bc87c9e61a4b60ffa965ba4bc45a48
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-01-19 22:30:15 +0300

    translated comments to english

--- telegram_bot_voice_to_text/voice_to_text_tg_bot.py
+++ telegram_bot_voice_to_text/voice_to_text_tg_bot.py
@@ -3,24 +3,21 @@ import telebot
 import speech_recognition
 from pydub import AudioSegment
 
-
-# reading the token
-with open ('token.txt', 'r') as token_file:
+# Reading the token
+with open('token.txt', 'r') as token_file:
     token = token_file.readline()
 
 bot = telebot.TeleBot(token)
 
-
 def oga_to_wav(filename):
-    # Конвертация формата файлов
+    # Convert file formats
     new_filename = filename.replace('.oga', '.wav')
     audio = AudioSegment.from_file(filename)
     audio.export(new_filename, format='wav')
     return new_filename
 
-
 def recognize_speech(oga_filename):
-    # Перевод голоса в текст + удаление использованных файлов
+    # Convert voice to text + delete used files
     wav_filename = oga_to_wav(oga_filename)
     recognizer = speech_recognition.Recognizer()
 
@@ -37,9 +34,8 @@ def recognize_speech(oga_filename):
 
     return text
 
-
 def download_file(bot, file_id):
-    # Скачивание файла, который прислал пользователь
+    # Download the file sent by the user
     file_info = bot.get_file(file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     filename = file_id + file_info.file_path
@@ -48,21 +44,17 @@ def download_file(bot, file_id):
         f.write(downloaded_file)
     return filename
 
-
 @bot.message_handler(commands=['start'])
 def say_hi(message):
-    # Функция, отправляющая "Привет" в ответ на команду /start
+    # Function sending "Hello" in response to the /start command
     bot.send_message(message.chat.id, 'Привет')
-
 
 @bot.message_handler(content_types=['voice'])
 def transcript(message):
-    # Функция, отправляющая текст в ответ на голосовое
+    # Function sending text in response to a voice message
     filename = download_file(bot, message.voice.file_id)
     text = recognize_speech(filename)
     bot.send_message(message.chat.id, text)
 
-
-# Запускаем бота.
-# Остановим ячейку - остановится бот
+# Run the bot.
 bot.polling()
