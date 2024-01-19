9cd03ee5137c2a7421b4b688ec6676b7002b280d
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-01-19 22:26:43 +0300

    made voice_to_text_tg.bot

---
+++ telegram_bot_voice_to_text/voice_to_text_tg_bot.py
@@ -0,0 +1,68 @@
+import os
+import telebot
+import speech_recognition
+from pydub import AudioSegment
+
+
+# reading the token
+with open ('token.txt', 'r') as token_file:
+    token = token_file.readline()
+
+bot = telebot.TeleBot(token)
+
+
+def oga_to_wav(filename):
+    # Конвертация формата файлов
+    new_filename = filename.replace('.oga', '.wav')
+    audio = AudioSegment.from_file(filename)
+    audio.export(new_filename, format='wav')
+    return new_filename
+
+
+def recognize_speech(oga_filename):
+    # Перевод голоса в текст + удаление использованных файлов
+    wav_filename = oga_to_wav(oga_filename)
+    recognizer = speech_recognition.Recognizer()
+
+    with speech_recognition.WavFile(wav_filename) as source:
+        wav_audio = recognizer.record(source)
+
+    text = recognizer.recognize_google(wav_audio, language='ru')
+
+    if os.path.exists(oga_filename):
+        os.remove(oga_filename)
+
+    if os.path.exists(wav_filename):
+        os.remove(wav_filename)
+
+    return text
+
+
+def download_file(bot, file_id):
+    # Скачивание файла, который прислал пользователь
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    filename = file_id + file_info.file_path
+    filename = filename.replace('/', '_')
+    with open(filename, 'wb') as f:
+        f.write(downloaded_file)
+    return filename
+
+
+@bot.message_handler(commands=['start'])
+def say_hi(message):
+    # Функция, отправляющая "Привет" в ответ на команду /start
+    bot.send_message(message.chat.id, 'Привет')
+
+
+@bot.message_handler(content_types=['voice'])
+def transcript(message):
+    # Функция, отправляющая текст в ответ на голосовое
+    filename = download_file(bot, message.voice.file_id)
+    text = recognize_speech(filename)
+    bot.send_message(message.chat.id, text)
+
+
+# Запускаем бота.
+# Остановим ячейку - остановится бот
+bot.polling()
